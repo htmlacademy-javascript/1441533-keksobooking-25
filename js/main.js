17ea662e41a824d17cd37d251076924fa7b1861d
tirion <tirionchen@mail.ru>
2022-02-21 21:18:34 +0300

    add

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 const randomNumber = (min, max) => {
   const rand = min - 0.5 + Math.random() * (max - min + 1);
-  if (min > max) {
+  if (min >= max) {
     return new Error('неверный числовой диапазон');
   }
   return Math.round(rand);
@@ -10,7 +10,7 @@ randomNumber(0, 3);
 
 const randomFloatNumber = (min, max, quantityNumber) => {
   const randomPoint = Math.random() * (max - min) + min;
-  if (min > max) {
+  if (min >= max) {
     return new Error('неверный числовой диапазон');
   }
   return randomPoint.toFixed(quantityNumber);
