6e0c4a0c7a33b41c0016f4c55ccee2b38873a52e
tirion <tirionchen@mail.ru>
2022-03-03 16:56:25 +0300

    fixX2

--- js/main.js
+++ js/main.js
@@ -111,44 +111,38 @@ const getRandomArrayLength = (array) => {
 };
 
 
-const getAuthor = () => {
+const getCardGenerate = () => {
+  const latCoordinate = getRandomFloatNumber(latLocation.MIN, latLocation.MAX, 5);
+  const lngCoordinate = getRandomFloatNumber(lngLocation.MIN, lngLocation.MAX, 5);
   const randomNum = getRandomNumber(AvatarRange.MIN, AvatarRange.MAX);
-  return ({
-    avatar: `img/avatars/user${randomNum === 10 ? '10': `0${randomNum}`}.png`,
-  });
+
+  return {
+    offer :{
+      title: getArrayRandElement(TITLE),
+      address: `${latCoordinate}, ${lngCoordinate}`,
+      price: getRandomNumber(PriceRange.MIN, PriceRange.MAX),
+      type: getArrayRandElement(TYPE),
+      rooms: getRandomNumber(RoomsRange.MIN, RoomsRange.MAX),
+      guests: getRandomNumber(GuestsRange.MIN, GuestsRange.MAX),
+      checking: getArrayRandElement(CHECKING),
+      checkout: getArrayRandElement(CHECKOUT),
+      features: getRandomArrayLength(FEATURES),
+      description: getArrayRandElement(DESCRIPTION),
+      photos: getRandomArrayLength(PHOTOS),
+    },
+
+    author: {
+      avatar: `img/avatars/user${randomNum === 10 ? '10': `0${randomNum}`}.png`,
+    },
+
+    location: {
+      lat: latCoordinate,
+      lng: lngCoordinate,
+    },
+  };
 };
 
 
-const getAddress = () => ({
-  lat: getRandomFloatNumber(latLocation.MIN, latLocation.MAX, 5),
-  lng: getRandomFloatNumber(lngLocation.MIN, lngLocation.MAX, 5),
-});
-
-
-const getAddresslat = getRandomFloatNumber(latLocation.MIN, latLocation.MAX, 5);
-const getAddresslng = getRandomFloatNumber(lngLocation.MIN, lngLocation.MAX, 5);
-
-
-const getCardGenerate = () => ({
-  title: getArrayRandElement(TITLE),
-  address: `${getAddresslat} ${getAddresslng}`,
-  price: getRandomNumber(PriceRange.MIN, PriceRange.MAX),
-  type: getArrayRandElement(TYPE),
-  rooms: getRandomNumber(RoomsRange.MIN, RoomsRange.MAX),
-  guests: getRandomNumber(GuestsRange.MIN, GuestsRange.MAX),
-  checking: getArrayRandElement(CHECKING),
-  checkout: getArrayRandElement(CHECKOUT),
-  features: getRandomArrayLength(FEATURES),
-  description: getArrayRandElement(DESCRIPTION),
-  photos: getRandomArrayLength(PHOTOS),
-});
-
-const getFiniteCard = () => ({
-  avatar: getAuthor(),
-  offer: getCardGenerate(),
-  location: getAddress(),
-});
-
-const createCard = () =>  new Array(10).fill(null).map(getFiniteCard);
+const createCard = () =>  new Array(10).fill(null).map(getCardGenerate);
 
 createCard();
