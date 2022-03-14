ca908299bc4b5f4ccfeee304b61dacc0cb87b341
tirion <tirionchen@mail.ru>
2022-03-14 03:37:03 +0300

    delete import

--- js/main.js
+++ js/main.js
@@ -1,10 +1,7 @@
 import {createCard} from './cardGenerate.js';
 import {generateCard} from './similarElements.js';
 
-import './data.js';
-import './util.js';
-import './cardGenerate.js';
-import './similarElements.js';
+
 import './form.js';
 
 const connectionArrayCard  = createCard();
