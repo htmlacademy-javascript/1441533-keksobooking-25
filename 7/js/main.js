7feb22149e4c4ddf2ac7b3ac5c7d92e3ac5d538c
Keks <keks@htmlacademy.ru>
2022-03-14 03:10:25 +0300

    :heavy_check_mark: Сборка #7

---
+++ 7/js/main.js
@@ -0,0 +1,11 @@
+import {createCard} from './cardGenerate.js';
+import {generateCard} from './similarElements.js';
+
+import './data.js';
+import './util.js';
+import './cardGenerate.js';
+import './similarElements.js';
+import './form.js';
+
+const connectionArrayCard  = createCard();
+generateCard(connectionArrayCard[0]);
