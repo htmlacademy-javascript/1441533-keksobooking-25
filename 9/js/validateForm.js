f17ae7425ef071a4177a5d2d00444c9bc431160d
Keks <keks@htmlacademy.ru>
2022-03-26 12:58:40 +0300

    :heavy_check_mark: Сборка #9

---
+++ 9/js/validateForm.js
@@ -0,0 +1,60 @@
+const form = document.querySelector('.ad-form');
+const rooms = document.querySelector('#room_number');
+const copacity = document.querySelector('#capacity');
+const price = form.querySelector('#price');
+const type = form.querySelector('#type');
+const checkInTime = form.querySelector('#timein');
+const departureTime = form.querySelector('#timeout');
+
+const PRICE_HOUSING = {
+  bungalow: 0,
+  flat: 1000,
+  hotel: 3000,
+  house: 5000,
+  palace: 10000,
+};
+
+const pristine = new Pristine(form, {
+  classTo: 'ad-form__element',
+  errorTextParent: 'ad-form__element',
+  errorTextTag: 'span',
+  errorTextClass: 'form__error',
+});
+
+const selectChangeArrive = () => {
+  checkInTime.value = departureTime.value;
+};
+
+const selectChangeExit = () => {
+  departureTime.value = checkInTime.value;
+};
+
+const selectChangeType = () => {
+  price.placeholder = PRICE_HOUSING[type.value];
+  price.min = PRICE_HOUSING[type.value];
+};
+
+pristine.addValidator(type, selectChangeType);
+pristine.addValidator(departureTime, selectChangeArrive);
+pristine.addValidator(checkInTime, selectChangeExit);
+
+const roomsOption = {
+  '1' : ['1'],
+  '2' : ['1', '2'],
+  '3' : ['1', '2', '3'],
+  '100' : ['0'],
+};
+
+const validateRoomsCopacity = () => roomsOption[rooms.value].includes(copacity.value);
+
+
+const getOptionErrorMessage = () => `${rooms.value === '100' ? '100 комнат только `не для гостей`' : 'нужно больше комнат'}`;
+
+
+pristine.addValidator(copacity, validateRoomsCopacity, getOptionErrorMessage);
+
+form.addEventListener('submit', (evt) => {
+  evt.preventDefault();
+  pristine.validate();
+});
+
